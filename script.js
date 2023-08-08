c03027892ef3fc90da3ab4e28b2aff737355ba26
Happy Patel <77920599+Happypatel3007@users.noreply.github.com>
2023-08-08 13:49:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Load library from the CDN
+var element = document.getElementById("element");
+
+    // Setup and start animation!
+    
+        var typed = new Typed(element, {
+            strings: ['Web Developer', 'Graphic Designer', 'UI Designer.'],
+            typeSpeed: 50,
+        });
